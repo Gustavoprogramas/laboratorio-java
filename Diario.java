801c1394b031807c341e5dc798a78580f51a4f83
Gustavoprogramas <gustavo.vimacedo@gmail.com>
2026-09-16 14:44:43 -0300

    Refactor nota type and add histogram generation
    Updated the nota type from double to int in modificarNota and related methods. Added gerarHistograma method to display frequency of grades.

--- Diario.java
+++ Diario.java
@@ -23,7 +23,7 @@ public class Diario {
 		}
 	}
 	
-	public void modificarNota(String nomeAluno, int numeroDaNota, double novaNota) {
+	public void modificarNota(String nomeAluno, int numeroDaNota, int novaNota) {
 		for (int cont = 0; cont < contadorDeAlunosCadastrados; cont++) {
 			if (listaDeAlunos[cont].getNomeAluno().equals(nomeAluno)) {
 				listaDeAlunos[cont].setNota(numeroDaNota - 1, novaNota);
@@ -45,7 +45,7 @@ public class Diario {
 		for (int cont = 0; cont < contadorDeAlunosCadastrados; cont++) {
 			Aluno a = listaDeAlunos[cont];
 		    System.out.print("Aluno " + a.getNomeAluno() + " | Notas: ");
-		    double[] notas = a.getNotasAluno();
+		    int[] notas = a.getNotasAluno();
 		    for (int j = 0; j < notas.length; j++) {
 		    	System.out.print(notas[j] + " | ");
 		    }
@@ -56,7 +56,7 @@ public class Diario {
 		double maior = 0;
 		double menor = 10;
 		for (int i = 0; i < contadorDeAlunosCadastrados; i++) {
-			double[] notas = listaDeAlunos[i].getNotasAluno();
+			int[] notas = listaDeAlunos[i].getNotasAluno();
 			for (int j = 0; j < notas.length; j++) {
 				if (notas[j] > maior) maior = notas[j];
 				if (notas[j] < menor) menor = notas[j];
@@ -64,5 +64,88 @@ public class Diario {
 		}
 		System.out.println("\nMaior nota: " + maior);
 		System.out.println("\nMenor nota: "+ menor);
+	}
+	
+	public void gerarHistograma() {
+		int intervalo[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+	
+		
+		String[] quadrado = {"▇"};
+		for (int i=0; i<contadorDeAlunosCadastrados; i++) {
+			int[] notas = listaDeAlunos[i].getNotasAluno();
+			for (int j = 0; j<notas.length; j++) {
+				if (notas[j] == 100) {
+					intervalo[9]++;
+				}else
+				intervalo[notas[j]/10]++;
+				
+			}
+		} //fim for alunos.lenght
+		System.out.printf("Frequencia de notas de 0 a 9: ");
+		for (int i=0; i<intervalo[0]; i++) {
+			System.out.printf("%s", quadrado);
+		}
+		System.out.printf(" %d notas", intervalo[0]);
+		System.out.println();
+		System.out.printf("Frequencia de notas de 10 a 19: ");
+		for (int i=0; i<intervalo[1]; i++) {
+			System.out.printf("%s", quadrado);
+		}
+		System.out.printf(" %d notas", intervalo[1]);
+		System.out.println();
+		System.out.printf("Frequencia de notas de 20 a 29: ");
+		for (int i=0; i<intervalo[2]; i++) {
+			System.out.printf("%s", quadrado);
+		}
+		System.out.printf(" %d notas", intervalo[2]);
+		System.out.println();
+		System.out.printf("Frequencia de notas de 30 a 39: ");
+		for (int i=0; i<intervalo[3]; i++) {
+			System.out.printf("%s", quadrado);
+		}
+		System.out.printf(" %d notas", intervalo[3]);
+		System.out.println();
+		System.out.printf("Frequencia de notas de 40 a 49: ");
+		for (int i=0; i<intervalo[4]; i++) {
+			System.out.printf("%s", quadrado);
+		}
+		System.out.printf(" %d notas", intervalo[4]);
+		System.out.println();
+		System.out.printf("Frequencia de notas de 50 a 59: ");
+		for (int i=0; i<intervalo[5]; i++) {
+			System.out.printf("%s", quadrado);
+		}
+		System.out.printf(" %d notas", intervalo[5]);
+		System.out.println();
+		System.out.printf("Frequencia de notas de 60 a 69: ");
+		for (int i=0; i<intervalo[6]; i++) {
+			System.out.printf("%s", quadrado);
+		}
+		System.out.printf(" %d notas", intervalo[6]);
+		System.out.println();
+		System.out.printf("Frequencia de notas de 70 a 79: ");
+		for (int i=0; i<intervalo[7]; i++) {
+			System.out.printf("%s", quadrado);
+		}
+		System.out.printf(" %d notas", intervalo[7]);
+		System.out.println();
+		System.out.printf("Frequencia de notas de 80 a 89: ");
+		for (int i=0; i<intervalo[8]; i++) {
+			System.out.printf("%s", quadrado);
+		}
+		System.out.printf(" %d notas", intervalo[8]);
+		System.out.println();
+		System.out.printf("Frequencia de notas de 90 a 100: ");
+		for (int i=0; i<intervalo[9]; i++) {
+		System.out.printf("%s", quadrado);
+		}
+		System.out.printf(" %d notas", intervalo[9]);
+		System.out.println();
+		
+		
+		
+		
+		
+		
 	}
 }
