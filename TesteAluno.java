ec5735934d9113c6fcd30a129993e5931c0f46d1
Gustavoprogramas <gustavo.vimacedo@gmail.com>
2026-09-16 14:44:31 -0300

    Refactor TesteAluno for improved user interaction

--- TesteAluno.java
+++ TesteAluno.java
@@ -8,9 +8,11 @@ public class TesteAluno {
 
         int alunos;
         int notas;
-        
+        System.out.println("Qual o nome da turma?");
+        String turma = input.nextLine();
         System.out.println("Quantos alunos tem na sala?");
         alunos = input.nextInt();
+      
         
         System.out.println("Quantas notas por aluno precisa?");
         notas = input.nextInt();
@@ -20,15 +22,21 @@ public class TesteAluno {
         
     
         input.nextLine(); 
-
+        for (int i = 0; i<alunos; i++) {
+        	System.out.print("Digite o nome do " + (i + 1) + "° aluno: ");
+        	String nomeDigitado = input.nextLine();
+        	meuDiario.adicionarAluno(nomeDigitado);
+        }
+        
         int opcao = -1; 
 
      
         while (opcao != 0) {
-            System.out.println("\n========= MENU =========");
+            System.out.printf("\n========= %s =========\n", turma);
             System.out.println("1 - Adicionar um novo aluno");
             System.out.println("2 - Adicionar/Editar a nota de um aluno");
             System.out.println("3 - Gerar relatório de notas");
+            System.out.println("4 - Gerar histograma da frequência de notas");
             System.out.println("0 - Sair do programa");
             System.out.print("Escolha uma opção: ");
             
@@ -48,15 +56,15 @@ public class TesteAluno {
                     System.out.print("\nDigite o nome do aluno que receberá a nota: ");
                     String nomeNota = input.nextLine();
                     
-                    System.out.print("Qual é o número da nota (Ex: 1, 2...)? ");
+                    System.out.print("Qual é o número da nota? ");
                     int numNota = input.nextInt();
                     
                     System.out.print("Digite o valor da nota: ");
-                    double valorNota = input.nextDouble();
+                    int valorNota = input.nextInt();
                     
                     
-                    if (valorNota > 10 || valorNota < 0) {
-                        System.out.println("Erro: Não pode nota maior que 10, nem menor que 0.");
+                    if (valorNota > 100 || valorNota < 0) {
+                        System.out.println("Erro: Não pode nota maior que 100, nem menor que 0.");
                     } else if (numNota < 1 || numNota > notas) {
                         System.out.println("Erro: Esse número de avaliação não existe.");
                     } else {
@@ -70,9 +78,12 @@ public class TesteAluno {
                     meuDiario.gerarRelatorioDeNotas();
                     meuDiario.relatorioMaiorNota();
                     break;
+                case 4:
+                	System.out.println();
+                	meuDiario.gerarHistograma();
                     
                 case 0:
-                    System.out.println("\nEncerrando o sistema. Até logo!");
+                    System.out.println("\nEncerrando o sistema...");
                     break;
                     
                 default:
